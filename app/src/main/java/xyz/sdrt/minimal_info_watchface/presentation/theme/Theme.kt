eccd3cd8ccd9bd856d29e650d2a66b021abd6ac6
kshksdrt <kaushik.sdrt@gmail.com>
2025-04-13 19:55:16 +0200

    An initial watchface

---
+++ app/src/main/java/xyz/sdrt/minimal_info_watchface/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package xyz.sdrt.minimal_info_watchface.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun MyApplicationTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
